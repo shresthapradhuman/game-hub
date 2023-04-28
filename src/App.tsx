850a1766bd60e00e67807b7f70f8d6d7c67cca35
shresthapradhuman <shresthapradhuman2018@gmail.com>
2023-04-28 23:05:27 +0900

    Displaying the Genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "280px 1fr",
+      }}
     >
       <GridItem area={"nav"}>
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"}>
+        <GridItem area={"aside"} paddingX={5}>
           <GenereList />
         </GridItem>
       </Show>
