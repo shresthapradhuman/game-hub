4605e62d772c134d78fe97d0b740c5c4f56bc565
shresthapradhuman <shresthapradhuman2018@gmail.com>
2023-05-06 11:29:38 +0900

    Handle Errors

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <Navbar />
+      <Box padding={5}>
+        <Heading>Oops</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "Page not found"
+            : "Something Went Wrong"}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
--- src/services/router.tsx
+++ src/services/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import LayoutPage from "../pages/LayoutPage";
 import HomePage from "../pages/HomePage";
 import GameDetailPage from "../pages/GameDetailPage";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LayoutPage />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
