89cde13d2534c9eaff8a576e9439b249439cf183
shresthapradhuman <shresthapradhuman2018@gmail.com>
2023-05-06 12:04:26 +0900

    fetch game details

--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -1,39 +1,13 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
-import ms from "ms";
-import APIClient, { FetchResponse } from "../services/api-client";
-import useGameQueryStore from "../store";
-import { Platform } from "./usePlatforms";
-
-export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-}
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { Game } from "./useGames";
 
 const apiClient = new APIClient<Game>("/games");
 
-const useGames = () => {
-  const gameQuery = useGameQueryStore((s) => s.gameQuery);
-  return useInfiniteQuery<FetchResponse<Game>, Error>({
-    queryKey: ["games", gameQuery],
-    queryFn: ({ pageParam = 1 }) =>
-      apiClient.getAll({
-        params: {
-          genres: gameQuery.genreId,
-          parent_platforms: gameQuery.platformId,
-          ordering: gameQuery.sortOrder,
-          search: gameQuery.searchText,
-          page: pageParam,
-        },
-      }),
-    staleTime: ms("24h"),
-    getNextPageParam: (lastPage, allPages) => {
-      return lastPage.next ? allPages.length + 1 : undefined;
-    },
+const useGame = (slug: string) =>
+  useQuery({
+    queryKey: ["games", slug],
+    queryFn: () => apiClient.get(slug),
   });
-};
 
-export default useGames;
+export default useGame;
--- src/pages/LayoutPage.tsx
+++ src/pages/LayoutPage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 
 const LayoutPage = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Box padding={5}>
+        <Outlet />
+      </Box>
     </>
   );
 };
